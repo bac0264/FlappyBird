b245c15bd1f45f2a83c1957859a19b1236ed3772
bac0264 <hjg4shjn0akazu12@gmail.com>
2018-02-28 01:04:05 +0700

    Update game FlappyBird

---
+++ Assets/Scripts/SpawnerPipeController/SpawnerPipe.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnerPipe : MonoBehaviour {
+    [SerializeField]
+    private GameObject pipeHolder;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Spawner());
+	}
+	
+	// render pipe
+	IEnumerator Spawner()
+    {
+        yield return new WaitForSeconds(1.2f);
+        Vector3 temp = transform.position;
+        temp.y = Random.Range(-2.5f, 2.5f);
+        Instantiate(pipeHolder,temp,Quaternion.identity);
+        StartCoroutine(Spawner());
+    }
+}
